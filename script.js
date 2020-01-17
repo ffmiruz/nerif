57b2864bfab161b0d3310b5c3b602f6a1248a867
ffmiyo <leffmiyo@gmail.com>
2020-01-18 00:31:23 +0800

    completed general layout

--- script.js
+++ script.js
@@ -7,9 +7,24 @@ const getPrices = async function() {
 	let request, data
 	//request = await fetch("http://cryptomarketplot.com/api.json", conf)
 	request = await fetch("/test_api.json", conf)
-	data = await request.json().then(arr => arr.slice(0,10))
-	btc = data[0]
-	current = btc.price_usd
-	$('.col.card span').textContent = current
+	data = await request.json().then(arr => arr.slice(1,11))
+
+	// 
+	data.forEach((item, i, array) => {
+	$a('.col.card h4')[i*2+1].textContent = item.symbol
+	$a('.col.card p')[i*2].textContent += item.price_btc
+	let algoPrice = expected(item.price_btc)
+	$a('.col.card p')[i*2+1].textContent += algoPrice.toExponential(4)
+	$a('.col.card h4')[i*2].textContent = diff(item.price_btc, algoPrice).toFixed(2) +'%'
+	});
+
+}
+
+const expected = function (arg){
+	return arg * 2.9 / 2.8
+}
+const diff = function (current, predicted){
+	return (current-predicted)/predicted
+	
 }
 getPrices();

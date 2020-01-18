ce36d571b9cbd1eb647e13672076511f4a8ddfda
ffmiyo <leffmiyo@gmail.com>
2020-01-18 21:52:14 +0800

    implement algos: ETH, XRP

--- script.js
+++ script.js
@@ -1,20 +1,21 @@
 const $ = _ => document.querySelector(_)
 const $a = _ => document.querySelectorAll(_)
+let btcusd
 
 const process = async function() {
 	//if( !arg ) return null;
 	const conf = {method: "GET"}
 	let request, data
-	request = await fetch("http://cryptomarketplot.com/api.json", conf)
-	//request = await fetch("/test_api.json", conf)
-	data = await request.json().then(arr => arr.slice(1,11))
-
+	//request = await fetch("http://cryptomarketplot.com/api.json", conf)
+	request = await fetch("/test_api.json", conf)
+	data = await request.json()
+	btcusd = data[0].price_usd
 	// 
-	data.forEach((item, i, array) => {
+	data.slice(1,11).forEach((item, i, array) => {
 	$a('.col.card h4')[i*2+1].textContent = item.symbol
 	$a('.col.card p')[i*2].textContent += item.price_btc
 
-	let algoPrice = runAlgo(item.symbol, item.price_btc)
+	let algoPrice = runAlgo(item.symbol, Number(btcusd))
 	if (algoPrice < 0) {
 		$a('.col.card p')[i*2+1].textContent += "Unavailable"
 		$a('.col.card h4')[i*2].textContent = "No results"
@@ -67,42 +68,52 @@ const runAlgo = function (symbol, price){
 	return predicted
 }
 
-const ETH = function (price) {
-	return price / 2.2*1.9
+const ETH = function (d) {
+	let y = 0.0
+	y = (((0.112774 + d) + ((0.112774 * 0.035685) - d)) + 0.112774)
+	y += 0.160917
+	y += ((((0.386812 / 0.350861) / (0.350861 * d)) / ((0.350861 - 0.386812) * 0.350861)) - 0.386812)
+	y += 0.043325
+	return y
 }
-const XRP = function (price) {
-	return price * 2/1.5
+const XRP = function (d) {
+	let y = 0.0
+	y = (((((0.187109 / d) / d) - d) + d) / 0.187109)
+	y += d
+	y += (d / (((0.249282 - d) / 0.249282) * ((d - d) - d)))
+	y += ((d * (d - 0.490774)) / (0.490774 - d))
+	return y
 }
-const BCH = function (price) {
-	return price * 2/1.9
+const BCH = function (d) {
+	return d * 2/1.9
 }
-const BSV = function (price) {
-	return price * 2.1/1.2
+const BSV = function (d) {
+	return d * 2.1/1.2
 }
-const LTC = function (price) {
-	return price / 1.3*1.8
+const LTC = function (d) {
+	return d / 1.3*1.8
 }
-const EOS = function (price) {
-	return price * 2/1.4
+const EOS = function (d) {
+	return d * 2/1.4
 }
-const BNB = function (price) {
-	return price * 2/1.1
+const BNB = function (d) {
+	return d * 2/1.1
 }
-const ETC = function (price) {
-	return price / 2*1.9
+const ETC = function (d) {
+	return d / 2*1.9
 }
-const XLM = function (price) {
-	return price * 2.3/1.9
+const XLM = function (d) {
+	return d * 2.3/1.9
 }
-const XMR = function (price) {
-	return price * 2.1/1.3
+const XMR = function (d) {
+	return d * 2.1/1.3
 }
 const pDefault = function () {
 	return  -1
 }
 
 const diff = function (current, predicted){
-	return (current-predicted)/predicted	
+	return (current-predicted)/predicted * 100	
 }
 
 
